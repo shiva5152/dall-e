75cfdf3cc0eb00c13fe1943e35d1d1d97eaf3bfc
shiva5152 <sg2304366@gmail.com>
2023-03-27 01:29:56 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.get('/',async(req,res)=>{
 app.use(errorMiddleware);
 
 // port and listing
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 const startServer=async ()=>{   
    try{
